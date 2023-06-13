2263f9a33f75744a86a4140024eaa4bacf05f778
Светлана Ременюк <lan2828@yandex.ru>
2023-06-13 12:52:41 +0300

    rm links

--- links.py
+++
@@ -1,8 +0,0 @@
-links = ('Please click on the club to book ✅\n\n'
-                             '[June 21, How to network?](https://events.webinar.ru/EGOUniversity/1701199985)\n\n'
-                                      '[July 6, The best education systems in the world](https://events.webinar.ru/EGOUniversity/535139667)\n\n'
-                             '[July 19, Cultural differences](https://events.webinar.ru/EGOUniversity/282361387)\n\n'
-                             '[August 1, Future of human space exploration](https://events.webinar.ru/EGOUniversity/492821355)\n\n'
-                             '[August 17, How would you define success? Can failures contribute to a person\'s success?]'
-                                      '(https://events.webinar.ru/EGOUniversity/545451806)\n\n'
-                             '[August 30, Self-learning systems](https://events.webinar.ru/EGOUniversity/1307025504)\n')
